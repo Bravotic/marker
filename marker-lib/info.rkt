0ec4e201e2cb30501798d5a5efa2ebdf2980a118
Collin McKinley <bravotic@aaathats3as.com>
2025-04-28 17:10:21 -0400

    Remove generator dependency from marker-lib

--- marker-lib/info.rkt
+++ marker-lib/info.rkt
@@ -1,5 +1,5 @@
 #lang info
 (define collection "marker")
-(define deps '("base" "marker-generators"))
+(define deps '("base"))
 (define verison "1.0")
 (define license "MIT")
--- marker-lib/main.rkt
+++ marker-lib/main.rkt
@@ -1,18 +1,7 @@
-(module marker racket
+#lang racket
 
 (require (for-syntax syntax/parse))
 (require marker/base marker/compiler)
-
-;; Generates a simple runner stub, allowing generators to be called by just running the Marker program. This will NOT
-;; run if required from another file or program.
-(define-for-syntax (make-generator-runtime page)
-  #'(module+ main
-      (require marker/runtime)
-      (display
-       (match (current-command-line-arguments)
-         [(vector format) (generate-page format page)]
-         [else (generate-page "text" page)]))))
-      
 
 ;; Entry point for a marker program when run or required in. Effectively compiles the entries to a variable named 'page'
 ;; which is exported.
@@ -21,24 +10,10 @@
     [(_ entries ...)
      #`(#%module-begin
         (define page (compile-entry entries ...))
-        (provide page)
-        #,(make-generator-runtime #'page))]))
-
-;; Each time a query is given to the REPL, this is called. Since Marker doesn't really have a REPL to speak of, we use
-;; this as a chance to ask the user for what format they want to export their page in. This is a workaround for now and
-;; I eventually hope to change it.
-(define-syntax (marker:#%top-interaction stx)
-  (syntax-parse stx
-    [(_ . format)
-     (let [(format-string (symbol->string (syntax->datum #'format)))]
-       #`(begin
-           (require (submod "."))
-           (require marker/repl)
-           (repl-generate-page #,format-string page)))]))
+        (provide page))]))
 
 (provide (rename-out
-          [marker:#%module-begin #%module-begin]
-          [marker:#%top-interaction #%top-interaction])
+          [marker:#%module-begin #%module-begin])
          #%datum
          #%top)
-)
+
