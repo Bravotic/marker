b13282e0b2b024deb793be71ea1388389fe32ac2
Collin McKinley <bravotic@aaathats3as.com>
2025-04-30 14:56:09 -0400

    Fix info files
    Hopefully we build now...

--- marker-lib/info.rkt
+++ marker-lib/info.rkt
@@ -1,5 +1,6 @@
 #lang info
 (define collection "marker")
 (define deps '("base"))
+(define build-deps '("rackunit"))
 (define verison "1.0")
 (define license "MIT")
--- marker/info.rkt
+++ marker/info.rkt
@@ -1,6 +1,7 @@
 #lang info
 (define collection 'multi)
 (define deps '("base" "marker-lib" "marker-generators"))
+(define build-deps '("racket-doc" "scribble-lib" "marker-lib"))
 (define implies '("marker-lib"))
 (define scribblings '(("scribblings/main.scrbl" () (experimental) "marker")))
 (define verison "1.0")
